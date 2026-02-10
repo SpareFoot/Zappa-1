c9a68573a2e3732e56a61fa35ba5b4964d2c0931
Nand Kishore Sagi <NandKishore.Sagi@storable.com>
2026-02-10 16:42:54 -0500

    Updated the package version and also tests for appropriate python version support. Jira ID: DATA-1431
    Signed-off-by: Nand Kishore Sagi <NandKishore.Sagi@storable.com>

--- zappa/__init__.py
+++ zappa/__init__.py
@@ -1,16 +1,14 @@
 import sys
 
-SUPPORTED_VERSIONS = [(3, 6), (3, 7), (3, 8)]
+MINIMUM_SUPPORTED_MINOR = 8
+MAXIMUM_SUPPORTED_MINOR = 13
 
-if sys.version_info[:2] not in SUPPORTED_VERSIONS:
-    formatted_supported_versions = [
-        "{}.{}".format(*version) for version in SUPPORTED_VERSIONS
-    ]
-    err_msg = "This version of Python ({}.{}) is not supported!\n".format(
-        *sys.version_info
-    ) + "Zappa (and AWS Lambda) support the following versions of Python: {}".format(
-        formatted_supported_versions
+if sys.version_info[0] != 3 or not (MINIMUM_SUPPORTED_MINOR <= sys.version_info[1] <= MAXIMUM_SUPPORTED_MINOR):
+    supported_range = f"3.{MINIMUM_SUPPORTED_MINOR}-3.{MAXIMUM_SUPPORTED_MINOR}"
+    current = f"{sys.version_info[0]}.{sys.version_info[1]}"
+    raise RuntimeError(
+        f"This version of Python ({current}) is not supported!\n"
+        f"Zappa (and AWS Lambda) support Python {supported_range}."
     )
-    raise RuntimeError(err_msg)
 
-__version__ = "0.53.0"
+__version__ = "0.54.0"
